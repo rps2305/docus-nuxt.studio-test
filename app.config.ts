37d64934f273054e57806c3d258a6e93f99ff0ce
rps2305 <73200332+rps2305@users.noreply.github.com>
2023-07-11 19:21:52 +0200

    image erbij

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@ export default defineAppConfig({
   docus: {
     title: 'Docus by Ronald Punt',
     description: 'The best place to start your documentation. Docus with  Nuxt 3 and Ronald Punt',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    image: '/INVA_102_Unit_12863R.jpg.webp',
 
     socials: {
       twitter: 'ronaldpunt',
